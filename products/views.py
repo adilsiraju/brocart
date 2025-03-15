248dbf816e8ca1078b78a3ede1c4ec8979641631
Mohammed Adil Siraju <mohdadilsiraju@gmail.com>
2025-03-15 23:27:04 +0530

    Enhance home page by adding featured and latest products dynamically

--- products/views.py
+++ products/views.py
@@ -5,7 +5,15 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    featured_products = Product.objects.order_by('-priority')[:4]
+    latest_products = Product.objects.order_by('-id')[:4]
+
+    context = {
+        'featured_products' : featured_products,
+        'latest_products' : latest_products
+    }
+
+    return render(request, 'pages/index.html', context)
 
 # Products list page
 def list_products(request):
